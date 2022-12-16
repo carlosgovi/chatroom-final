5480fff28a7eb74109faaefe14f477248239bcfd
carlosgovi <cgovi66@gmail.com>
2022-12-16 20:47:07 -0300

    cambios en db varibles de entorno

--- src/back/db.ts
+++ src/back/db.ts
@@ -1,14 +1,27 @@
 import * as admin from "firebase-admin";
 // import * as serviceAccount from "./key.json";
 ///////////
-console.log("variables::::", process.env);
+console.log("variables::::", process.env, typeof process.env);
 ////////////
 ///utilizo las variables de entorno y no uso mas el json
-const variablesDeEntorno = process.env;
+const variablesDeEntorno: any = process.env;
+const firebaseConfig: any = {
+  type: variablesDeEntorno.TYPE,
+  proyect_id: variablesDeEntorno.PROYECT_ID,
+  private_key_id: variablesDeEntorno.PRIVATE_KEY_ID,
+  private_key: variablesDeEntorno.PRIVATE_KEY,
+  cliente_email: variablesDeEntorno.CLIENT_EMAIL,
+  cliente_id: variablesDeEntorno.CLIENT_ID,
+  auth_uri: variablesDeEntorno.AUTH_URI,
+  token_uri: variablesDeEntorno.TOKEN_URI,
+  auth_provider_X509_cert_url: variablesDeEntorno.AUTH_PROVIDER_X509_CERT_URL,
+  client_X509_cert_url: variablesDeEntorno.CLIENT_X509_CERT_URL,
+  enviroment: variablesDeEntorno.ENVIROMENT,
+};
 // var serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(variablesDeEntorno as any),
+  credential: admin.credential.cert(JSON.stringify(firebaseConfig)),
   databaseURL: "https://apx-dwfs-m6-default-rtdb.firebaseio.com",
 });
 //referencia a la bdreal time
