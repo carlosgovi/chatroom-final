86107aa634c49ebbcb406044e8e0e2d6aaf17d03
carlosgovi <cgovi66@gmail.com>
2022-12-16 22:43:48 -0300

    cambio en objeto para las variables de entorno

--- src/back/db.ts
+++ src/back/db.ts
@@ -7,21 +7,23 @@ console.log("variables::::", process.env, typeof process.env);
 const variablesDeEntorno: any = process.env;
 const firebaseConfig: any = {
   type: variablesDeEntorno.TYPE,
-  proyect_id: variablesDeEntorno.PROYECT_ID,
+  project_id: variablesDeEntorno.PROYECT_ID,
   private_key_id: variablesDeEntorno.PRIVATE_KEY_ID,
   private_key: variablesDeEntorno.PRIVATE_KEY,
-  cliente_email: variablesDeEntorno.CLIENT_EMAIL,
-  cliente_id: variablesDeEntorno.CLIENT_ID,
+  client_email: variablesDeEntorno.CLIENT_EMAIL,
+  client_id: variablesDeEntorno.CLIENT_ID,
   auth_uri: variablesDeEntorno.AUTH_URI,
   token_uri: variablesDeEntorno.TOKEN_URI,
   auth_provider_X509_cert_url: variablesDeEntorno.AUTH_PROVIDER_X509_CERT_URL,
   client_X509_cert_url: variablesDeEntorno.CLIENT_X509_CERT_URL,
   enviroment: variablesDeEntorno.ENVIROMENT,
 };
+console.log("objeto variables pasadas", firebaseConfig);
+
 // var serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.stringify(firebaseConfig)),
+  credential: admin.credential.cert(firebaseConfig),
   databaseURL: "https://apx-dwfs-m6-default-rtdb.firebaseio.com",
 });
 //referencia a la bdreal time
